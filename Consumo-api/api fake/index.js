129c3d25746092ca386806e7d3aecc33cf2c816b
Darlon <darloncosta050@gmail.com>
2026-03-18 14:32:03 -0300

    teste ok

--- Consumo-api/api fake/index.js
+++ Consumo-api/api fake/index.js
@@ -1,6 +1,20 @@
+const item = document.getElementById("item");
 
+async function getProduct() {
+  try {
+    const response = await fetch("db.json");
+    const data = await response.json();
 
+    item.innerHTML = data.map(produto => `
+      <div class="produto">
+        <h3>${produto.nome}</h3>
+        <p>Preço: R$ ${produto.preco}</p>
+        <img src="${produto.imagem}" alt="${produto.nome}" width="100">
+      </div>
+    `).join('')
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-
-
-
+getProduct();
