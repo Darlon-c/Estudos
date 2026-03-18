e4c38a16795249d1191b06d3cadd80c5ffb86d16
Darlon <darloncosta050@gmail.com>
2026-03-18 14:13:17 -0300

    att

--- Consumo-api/fake-store/index.js
+++
@@ -1,9 +0,0 @@
-const productList = document.getElementById("productList");
-const cartList = document.getElementById("cartList");
-const cartTotal = document.getElementById("cartTotal");
-const checkoutBtn = document.getElementById("checkoutBtn");
-
-
-
-
-
